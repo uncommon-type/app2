006b708078b8fdb1c87633f7df68ed365d544c33
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-13 21:54:22 +0300

    Add file

---
+++ src/components/ArtistImage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArtistImage = () => (
+  <div className="info__item box">
+    <div className="info__item__image stack">
+      <h3 className="info__item__header text-500">Madonna</h3>
+      <img
+        className="image__item"
+        src="https://i.scdn.co/image/96b4818a65820e91e0e17fcf55a4d2213b019ad4"
+        alt="Madonna"
+      />
+    </div>
+  </div>
+);
+
+export default ArtistImage;
