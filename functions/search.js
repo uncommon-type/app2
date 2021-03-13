c732397c6b2864d7a029f3d5802fa09d39c599de
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-13 22:59:21 +0300

    Get artist out of query string parameters

--- functions/search.js
+++ functions/search.js
@@ -2,7 +2,8 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 
 const fetch = require("node-fetch");
 
-exports.handler = async () => {
+exports.handler = async ({ queryStringParameters }) => {
+  const { artist } = queryStringParameters;
   return {
     statusCode: 200,
     body: "",
