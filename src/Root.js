d619f1c9d3d76868e851880e01e7060c61635a0d
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-13 21:34:26 +0300

    Add instance of SearchBar

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Root() {
-  return <div>hello</div>;
-}
+const Root = () => {
+  return (
+    <main className="app">
+      <section className="app__section">
+        <SearchBar />
+      </section>
+    </main>
+  );
+};
 
 export default Root;
