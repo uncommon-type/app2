9d03ed87b0f93c060b3e490015f7a395edbbab44
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-14 21:58:03 +0300

    Make form use onSubmit event , make onSubmit handleSearchSubmit

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -2,11 +2,12 @@ import React, { Fragment, useState } from "react";
 
 const SearchBar = () => {
   const [inputValue, setInputValue] = useState("");
+
   return (
     <Fragment>
       <h3>Search for an Artist</h3>
       <div className="section__form splitter">
-        <form className="search-form">
+        <form onSubmit={handleSearchSubmit} className="search-form">
           <input
             type="text"
             name="q"
