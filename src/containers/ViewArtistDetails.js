44d5bf49b161eca7ef173e6527d2ffb2afb39232
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 18:52:57 +0300

    Add isLoading, isSuccess, isError variables

--- src/containers/ViewArtistDetails.js
+++ src/containers/ViewArtistDetails.js
@@ -8,6 +8,10 @@ const ViewArtistDetails = () => {
   const [status, setStatus] = useState("idle");
   const [error, setError] = useState(null);
 
+  const isLoading = status === "loading";
+  const isSuccess = status === "success";
+  const isError = status === "error";
+
   return (
     <div className="splitter2 gap-top">
       <article className="artist-infogroup flow radius">
