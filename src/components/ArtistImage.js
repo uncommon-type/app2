921694a385b34798fe2f3f31b98e6072fc505658
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 20:43:43 +0300

    Give ArtistImage artist prop, replace static data with dynamic one

--- src/components/ArtistImage.js
+++ src/components/ArtistImage.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ArtistImage = () => (
+const ArtistImage = ({ artist }) => (
   <div className="info__item box">
     <div className="info__item__image stack">
       <h3 className="info__item__header text-500">Madonna</h3>
       <img
         className="image__item"
-        src="https://i.scdn.co/image/96b4818a65820e91e0e17fcf55a4d2213b019ad4"
-        alt="Madonna"
+        src={artist?.images[0]?.url || "https://placehold.it/640x640"}
+        alt={artist.name}
       />
     </div>
   </div>
