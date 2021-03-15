196b339b84b95fdd76f3c568822937ae5e571be7
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 19:47:29 +0300

    Import Spinner

--- src/containers/ViewArtistDetails.js
+++ src/containers/ViewArtistDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import Spinner from "../components/Spinner";
 import RelatedArtistsList from "../components/RelatedArtistsList";
 import TopTracksList from "../components/TopTracksList";
 import ArtistImage from "../components/ArtistImage";
