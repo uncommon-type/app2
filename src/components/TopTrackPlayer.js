6ea2bf329dab55f6ecd962c5c42a84c6a0d57ec2
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 22:52:14 +0300

    Pass track prop to TopTrackPlayer, replace hard-coded data with dynamic one

--- src/components/TopTrackPlayer.js
+++ src/components/TopTrackPlayer.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const TopTrackPlayer = () => {
+const TopTrackPlayer = ({ track }) => {
   return (
     <div className="player_container">
       <audio
         className="player"
-        src="https://p.scdn.co/mp3-preview/bf54eab00d276b376af52d0de2b3df1b8a21c1eb?cid=3298c471c845493c9f32c9ead5100863"
+        src={track.preview_url}
         controls
         loop=""
       ></audio>
