5f5a855d05a7d08a4a014781705bb264d50853a7
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 18:06:45 +0300

    Import SearchResultsTableRow and Arrow components

---
+++ src/components/SearchResultsTable.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import SearchResultsTableRow from "../components/SearchResultsTableRow";
+import Arrow from "../components/Arrow";
+
+const SearchResultsTable = ({ artists, searchTerm }) => {};
+
+export default SearchResultsTable;
