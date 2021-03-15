af78020545dcc012a8112123263de906bd435335
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 16:58:48 +0300

    Import useLocation from react-router-dom

--- src/containers/ViewSearchResults.js
+++ src/containers/ViewSearchResults.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import Arrow from "../components/Arrow";
 import SearchResultsTableRow from "../components/SearchResultsTableRow";
