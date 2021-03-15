edb89709ee18ed18cec780b4cdc841544fdc21a3
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 18:10:57 +0300

    Add table headers array

--- src/components/SearchResultsTable.js
+++ src/components/SearchResultsTable.js
@@ -3,6 +3,8 @@ import React from "react";
 import SearchResultsTableRow from "../components/SearchResultsTableRow";
 import Arrow from "../components/Arrow";
 
-const SearchResultsTable = ({ artists, searchTerm }) => {};
+const SearchResultsTable = ({ artists, searchTerm }) => {
+  const tableHeaders = ["name", "genre", "followers", "popularity"];
+};
 
 export default SearchResultsTable;
