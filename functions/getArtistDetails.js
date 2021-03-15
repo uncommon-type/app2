bed5fd23ed2e35f09a7b9a9e18ed1054274a3e9c
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 20:31:24 +0300

    Add getRefreshToken function

--- functions/getArtistDetails.js
+++ functions/getArtistDetails.js
@@ -2,6 +2,23 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 
 const fetch = require("node-fetch");
 
+const getRefreshToken = async () => {
+  try {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${process.env.BASE_64_ENCODED_CLIENT_CREDENTIALS}`,
+      },
+      body: "grant_type=client_credentials",
+    });
+    const data = await result.json();
+    return data.access_token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 exports.handler = async () => {
   return {
     statusCode: 200,
