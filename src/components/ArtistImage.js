0bafd6064e3979e3455e91c9c4ca0ce43056b0d4
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 21:55:28 +0300

    Replace hard-coded data with dynamic one

--- src/components/ArtistImage.js
+++ src/components/ArtistImage.js
@@ -3,7 +3,7 @@ import React from "react";
 const ArtistImage = ({ artist }) => (
   <div className="info__item box">
     <div className="info__item__image stack">
-      <h3 className="info__item__header text-500">Madonna</h3>
+      <h3 className="info__item__header text-500">{artist.name}</h3>
       <img
         className="image__item"
         src={artist?.images[0]?.url || "https://placehold.it/640x640"}
