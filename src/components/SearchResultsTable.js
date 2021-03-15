2b3312cc55f819557818c63426e94b8413e14349
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-15 18:12:25 +0300

    Generate table headers

--- src/components/SearchResultsTable.js
+++ src/components/SearchResultsTable.js
@@ -5,6 +5,40 @@ import Arrow from "../components/Arrow";
 
 const SearchResultsTable = ({ artists, searchTerm }) => {
   const tableHeaders = ["name", "genre", "followers", "popularity"];
+
+  return (
+    <div className="artist-list-view flow radius">
+      <h4>Artists with the name of: {searchTerm}</h4>
+      <small className="scroll-message">(scroll to see more)</small>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map((header, i) =>
+              i === tableHeaders.length - 1 ? (
+                <th
+                  scope="col"
+                  key={i}
+                  aria-sort="none"
+                  className="popularity-col"
+                >
+                  {header}
+                  <button className="sorting-button">
+                    <Arrow />
+                  </button>
+                </th>
+              ) : (
+                <th scope="col" key={i}>
+                  {header}
+                </th>
+              )
+            )}
+          </tr>
+        </thead>
+
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 };
 
 export default SearchResultsTable;
