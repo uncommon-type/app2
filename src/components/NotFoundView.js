5dc65ee79a1ed3299e4825f5366df8c72306f7bc
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-16 14:59:51 +0300

    Remove file

--- src/components/NotFoundView.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFoundView = () => (
-  <div className="not-found-view">
-    <div>Sorry... nothing here. Go Home</div>
-  </div>
-);
-
-export default NotFoundView;
