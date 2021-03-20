ef80bf288672ec153f3465bf99bbea6ce38b9103
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-20 17:24:22 +0300

    Remove function block body

--- src/components/TopTrackPlayer.js
+++ src/components/TopTrackPlayer.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-const TopTrackPlayer = ({ track }) => {
-  return (
-    <div className="player_container">
-      <audio
-        className="player"
-        src={track.preview_url}
-        controls
-        loop=""
-      ></audio>
-    </div>
-  );
-};
+const TopTrackPlayer = ({ track }) => (
+  <div className="player_container">
+    <audio className="player" src={track.preview_url} controls loop=""></audio>
+  </div>
+);
 
 export default TopTrackPlayer;
--- src/components/ViewNotFound.js
+++ src/components/ViewNotFound.js
@@ -2,16 +2,15 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-const NotFoundView = () => {
-  return (
-    <div className="not-found-view">
-      <div>
-        Sorry... nothing here.
-        <Link to="/">
-          Go <strong>Home</strong>
-        </Link>
-      </div>
+const NotFoundView = () => (
+  <div className="not-found-view">
+    <div>
+      Sorry... nothing here.
+      <Link to="/">
+        Go <strong>Home</strong>
+      </Link>
     </div>
-  );
-};
+  </div>
+);
+
 export default NotFoundView;
