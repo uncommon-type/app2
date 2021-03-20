0294e2ce901d3c0c865be20bad22d0283c69834e
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-20 17:49:35 +0300

    Import useState from react

--- src/components/SearchResultsTable.js
+++ src/components/SearchResultsTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SearchResultsTableRow from "../components/SearchResultsTableRow";
 import Arrow from "../components/Arrow";
