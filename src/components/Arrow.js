b70f68828aff30578fcf7f90c5c0098763d299e4
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-20 17:54:39 +0300

    If sort direction is ascending, show an up-arrow

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Arrow = ({ sortDir }) => (
   <svg viewBox="0 0 100 200" width="100" height="200">
+    {!sortDir && <polyline points="20 50, 50 20, 80 50" />}
     <line x1="50" y1="20" x2="50" y2="180"></line>
   </svg>
 );
