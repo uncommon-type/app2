f38b5d736717865e36ff833622c323b3f990794d
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-20 17:55:11 +0300

    If sort direction is descending, show a down-arrow

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -4,6 +4,7 @@ const Arrow = ({ sortDir }) => (
   <svg viewBox="0 0 100 200" width="100" height="200">
     {!sortDir && <polyline points="20 50, 50 20, 80 50" />}
     <line x1="50" y1="20" x2="50" y2="180"></line>
+    {sortDir && <polyline points="20 150, 50 180, 80 150" />}
   </svg>
 );
 
